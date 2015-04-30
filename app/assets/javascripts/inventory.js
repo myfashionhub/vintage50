199c5c39b6a38b6213ce5809c0f3f53e90a118d4
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-04-30 00:07:34 -0400

    Add bulk edit actions

--- app/assets/javascripts/inventory.js
+++ app/assets/javascripts/inventory.js
@@ -1,10 +1,19 @@
-var Item = function(id, element) {
+$(document).ready(function() {
+  $('.product .actions').on('change', function(e) {  
+  	var action = e.target.value; 
+  	var item = new Item($(e.target).parent()); 
+  	item.changeStatus(action);
+  });
+
+  var inventory = new Inventory();
+});
+
+var Item = function(element) {
 	var that = this;
-	this.id = id;
 	this.element = element;
+	this.id = element.find('.meta').attr('data-id');
 
 	this.changeStatus = function(action) {
-		console.log(action)
 		if (action === 'delete') {
 			that.removeFromInventory();
 		} else if (action === 'publish') {
@@ -39,4 +48,23 @@ var Item = function(id, element) {
 			}
 		});		
 	}
+}
+
+var Inventory = function() {
+	this.container = $('.bulk-edit');
+  this.selectAllButton = this.container.find('.select-all');
+  this.deselectAllButton = this.container.find('.deselect-all');
+  this.actions = this.container.find('.bulk-actions');
+
+  this.init = function() {
+  	this.selectAllButton.click(function() {
+  		$('.product .checkbox').prop('checked', true);
+  	});
+
+  	this.deselectAllButton.click(function() {
+  		$('.product .checkbox').prop('checked', false);
+  	});
+  };
+
+  this.init();
 }
